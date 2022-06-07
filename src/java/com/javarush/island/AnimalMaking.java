db00bb78b8832c341f2b7f0abf030507b46453ec
DanirPT <danir.hucaev@gmail.com>
2022-06-07 10:18:39 +0100

    Now AnimalMaking.java makes animal's objects

--- src/java/com/javarush/island/AnimalMaking.java
+++ src/java/com/javarush/island/AnimalMaking.java
@@ -2,6 +2,8 @@ package com.javarush.island;
 
 import com.javarush.island.Annotations.CharacteristicsOfAnimal;
 import com.javarush.island.Annotations.Emoji;
+import com.javarush.island.Carnivores.*;
+import com.javarush.island.Herbivores.*;
 import com.javarush.island.abstractClasses.Animal;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -51,14 +53,41 @@ public class AnimalMaking {
 
     public int goCreate(Class animal, String[][] island){
         ThreadLocalRandom randomPosition = ThreadLocalRandom.current();
+
         int counter = 0;
         for (int i = 0; i < randomPosition.nextInt(1, animalQuantity(animal)); i++){
             int x = randomPosition.nextInt(0, 100);
             int y = randomPosition.nextInt(0, 20);
+            Animal newAnimal = switch (animal.getSimpleName()) {
+                case "Bear" -> new Bear(x, y);
+                case "Boa" -> new Boa(x, y);
+                case "Eagle" -> new Eagle(x, y);
+                case "Fox" -> new Fox(x, y);
+                case "Wolf" -> new Wolf(x, y);
+                case "Boar" -> new Boar(x, y);
+                case "Buffalo" -> new Buffalo(x, y);
+                case "Caterpillar" -> new Caterpillar(x, y);
+                case "Deer" -> new Deer(x, y);
+                case "Duck" -> new Duck(x, y);
+                case "Goat" -> new Goat(x, y);
+                case "Horse" -> new Horse(x, y);
+                case "Mouse" -> new Mouse(x, y);
+                case "Rabbit" -> new Rabbit(x, y);
+                case "Sheep" -> new Sheep(x, y);
+                default -> throw new RuntimeException("It is not an animal!");
+            };
+
             island[x][y] = createAnimalEmoji(animal);
             ++counter;
         }
         System.out.print(animal.getSimpleName() + " = ");
         return counter;
     }
+//
+//    public Animal animalConveyor(Class animal){
+//
+//        switch (animal.getSimpleName()){
+//            case "Bear":
+//        }
+//    }
 }
